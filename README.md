3b64946deb5a512ec2f5f5ae681216704d3aba48
Yale Cason <yale.cason@gmail.com>
2015-05-03 20:39:32 -0400

    Simplify pushing gh-pages with a shell script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rust-intro
 Tampa Code Camp talk - Intro to Rust
+
+Presentation can be viewed at (http://ghotiphud.github.io/rust-intro)
---
+++ push-gh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#pushes the presentation directory to the gh-pages branch.
+
+git subtree push --prefix presentation origin gh-pages
