8504ff231bb86086b4953b491f4aa62bbd85e9fd
ghotiphud <yale.cason@gmail.com>
2015-07-15 16:20:03 -0400

    Add concurrency section to presentation

--- src/concurrent.rs
+++ src/concurrent.rs
@@ -1,3 +1,4 @@
+#![feature(core_intrinsics)]
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc;
 use std::thread;
@@ -9,32 +10,33 @@ fn main() {
 
     mutex_locking();
 
-    mutex_locking_channels();
+    mutex_locking_with_channel();
 }
 
 fn print_from_thread() {
-    //Send and Sync
+    // spawn returns a JoinHandle which will wait until thread completion.
     let handle = thread::spawn(|| {
         println!("Hello from a thread!");
     });
 
+    // we wait here for the thread to complete.
     handle.join().unwrap();
 }
 
 fn return_from_thread() {
-    //Send and Sync
+    // JoinHandle<&str> here 
     let handle = thread::spawn(|| {
         "Returned from a thread!"
     });
 
+    // wait for thread and retrieve the result
     let val = handle.join().unwrap();
-
     println!("{}", val);
 }
 
 fn mutex_locking() {
+    // Tricky type: Arc is "Send", Mutex protects shared mutability
     let arc_data = Arc::new(Mutex::new(vec![1u32, 2, 3]));
-
     let mut handles: Vec<thread::JoinHandle<()>> = Vec::new();
 
     for i in 0..3 {
@@ -43,27 +45,22 @@ fn mutex_locking() {
             let mut unlocked_data = mutex_data.lock().unwrap();
             unlocked_data[i] += 1;
         });
-
         handles.push(handle);
     }
 
     handles.into_iter().map(|h| h.join().unwrap()).collect::<Vec<_>>();
-
     println!("data: {:?}", *arc_data.lock().unwrap());
 }
 
-fn mutex_locking_channels() {
+fn mutex_locking_with_channel() {
     let arc_data = Arc::new(Mutex::new(0u32));
-
     let (tx, rx) = mpsc::channel();
 
     for _ in 0..10 {
         let (mutex_data, thread_tx) = (arc_data.clone(), tx.clone());
-
         thread::spawn(move || {
             let mut unlocked_data = mutex_data.lock().unwrap();
             *unlocked_data += 1;
-
             thread_tx.send(()).unwrap();
         });
     }
@@ -71,6 +68,15 @@ fn mutex_locking_channels() {
     for _ in 0..10 {
         rx.recv().unwrap();
     }
-
     println!("{:?}", *arc_data.lock().unwrap());
+}
+
+
+
+fn print_type_of<T>(_: &T) -> () {
+    let type_name =
+        unsafe {
+            std::intrinsics::type_name::<T>()
+        };
+    println!("{}", type_name);
 }
